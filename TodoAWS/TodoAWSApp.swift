48ede1ab4050292c1806d92bf45fef89e1acd1d2
Raphael Iyin <iyinraphael@gmail.com>
2023-07-18 16:17:13 -0400

    initial commit

---
+++ TodoAWS/TodoAWSApp.swift
@@ -0,0 +1,37 @@
+//
+//  TodoAWSApp.swift
+//  TodoAWS
+//
+//  Created by Raphael Iyin on 7/18/23.
+//
+
+import SwiftUI
+import Amplify
+import AWSDataStorePlugin
+
+@main
+struct TodoAWSApp: App {
+    
+    init() {
+        configureAmplify()
+    }
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+    
+
+    func configureAmplify() {
+        let dataStorePlugin = AWSDataStorePlugin(modelRegistration: AmplifyModels())
+        do {
+            try Amplify.add(plugin: dataStorePlugin)
+            try Amplify.configure()
+            print("Initialized Amplify")
+        } catch {
+            print("could not initialize Amplify: \(error)")
+        }
+    }
+    
+}
